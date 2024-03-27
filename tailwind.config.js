5280eeb5e68f5cfa115fe2b74e76a1db308da57f
Jgarette0 <aajiliangarette@gmail.com>
2024-03-27 21:31:02 +0800

    :zap:

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,39 +4,41 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
-        'dahlia': ['Dahlia Medium', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
+        dahlia: ["Dahlia Medium", "sans-serif"],
       },
-      
+
       rotate: {
-        '360': '360deg',
+        360: "360deg",
       },
       fontSize: {
-        '6.5xl' : '3rem',
-        '6xl': '3.5rem', // Adjust the value as needed
-        '7xl': '9rem'
+        "6.5xl": "3rem",
+        "6xl": "3.5rem", // Adjust the value as needed
+        "6.8xl": "6.3rem",
+        "7xl": "9rem",
       },
       colors: {
         beige: "#ebdbc2",
         gold: "#d4b34c",
-        plant: "#0d0c22",
+        plant: "#222831",
         plantito: "#232329",
-        lightgold : '#dcbc53',
-        'selection': '#ff0000'
-
+        lightgold: "#dcbc53",
+        peach: "#ffda79",
+        selection: "#ff0000",
       },
       height: {
-        'titlesm' : '15rem',
-        'title' : '28rem',
-        'h-100': '36rem',
-        'h-150': '49rem',        
-        'h-125': '44rem',
+        titlesm: "15rem",
+        title: "28rem",
+        "h-100": "36rem",
+        "h-150": "49rem",
+        "h-125": "44rem",
       },
       width: {
-        'w-100': '28rem',
-        '1k': '1026px',
-        '4h' : '480px',
-        '3.1h': '330px'
+        "w-100": "28rem",
+        "1k": "1026px",
+        "4h": "480px",
+        "3.1h": "330px",
+        wow: "600px",
       },
     },
   },
